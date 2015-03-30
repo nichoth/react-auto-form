9d4cbe3f46e4d395391980ade4b10d36d631b04f
nichoth <nichoth@gmail.com>
2015-03-30 16:51:36 -0700

    refactor

--- Form.react.js
+++ Form.react.js
@@ -1,102 +1,18 @@
 var React = require('react'),
+    Facet = require('./Facet.react'),
     mui = require('material-ui'),
-    Paper = mui.Paper,
-    TextField = mui.TextField;
-
-var Facet = React.createClass({
-
-  propTypes: {
-    onDelete: React.PropTypes.func,
-    onComplete: React.PropTypes.func,
-    defaultContent: React.PropTypes.object
-  },
-
-  clearValue: function() {
-    this.refs.field.clearValue();
-    this.refs.value.clearValue();
-  },
-
-  focus: function() {
-    this.refs.field.focus();
-  },
-
-  getContent: function() {
-    return {
-      field: this.refs.field.getValue(),
-      value: this.refs.value.getValue()
-    }
-  },
-
-  fieldKeyDownHandler: function(ev) {
-    // backspace
-    var pair = this.getContent();
-    if (pair.field || pair.value) return;
-    if ( ev.keyCode == 8 && !(this.refs.field.getValue()) ) {
-      this.props.onDelete(pair, this);
-      ev.preventDefault();
-    }
-  },
-
-  valueKeyDownHandler: function(ev) {
-    if ( event.keyCode == 9 && !(event.shiftKey) && this.props.onComplete ) {
-      event.preventDefault();
-      var pair = {
-        field: this.refs.field.getValue(),
-        value: this.refs.value.getValue(),
-      };
-      this.props.onComplete(pair, event);
-    }
-
-    // backspace
-    if ( ev.keyCode == 8 && !(this.refs.value.getValue()) ) {
-      this.refs.field.focus();
-      ev.preventDefault();
-    }
-  },
-
-  render: function() {
-
-    var styles = {
-      padding: "1em",
-      display: "inline-block",
-      maxWidth: "50%"
-    };
-
-    var content = this.props.defaultContent || {field: null, value: null};
-
-    return (
-      <div>
-        <div className="test-field" style={styles}>
-          <TextField
-            onKeyDown={this.fieldKeyDownHandler}
-            ref="field"
-            hintText="Hint Text"
-            defaultValue={content.field}
-          />
-        </div>
-        <div className="test-field" style={styles}>
-          <TextField
-            ref="value"
-            onKeyDown={this.valueKeyDownHandler}
-            hintText="Hint Text"
-            defaultValue={content.value}
-          />
-        </div>
-      </div>
-    );
-  }
-});
-
+    Paper = mui.Paper;
 
 var Form = React.createClass({
 
   getInitialState: function() {
     return {
-      rows: [{field: '', value: ''}]
+      rows: [true]
     };
   },
 
   onFacetComplete: function(pair) {
+    console.log(this.refs.form.children);
     if (pair.field && pair.value) {
       var state = this.state;
       state.rows.push(pair);
@@ -132,7 +48,7 @@ var Form = React.createClass({
 
     return (
       <Paper className="test" zDepth={1}>
-        <div className="form">
+        <div ref="form" className="form">
           {rows}
           <Facet
             ref={'empty'}
--- example/example.react.js
+++ example/example.react.js
@@ -1,4 +1,30 @@
 var React = require('react');
 var Form = require('../Form.react');
+var Facet = require('../Facet.react');
 window.React = React;
+
+var React = require('react');
+
+var Example = React.createClass({
+
+  getInitialState: function() {
+    return {
+      rows: []
+    };
+  },
+
+  render: function() {
+    var rows = this.state.rows.map(function(row) {
+      return
+    }, this);
+
+    return (
+      <Form ref="form">
+        <Facet onComplete={this.onFacetComplete} />
+      </Form>
+    );
+  }
+
+});
+
 React.render(<Form />, document.body);
